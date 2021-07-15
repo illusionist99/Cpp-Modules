b3895832c8f0d94c2aee0fba64ad14cc8cdb48be
Mohamed Alaoui <malaoui@e0r2p3.1337.ma>
2021-07-15 20:02:13 +0100

    h

--- module05/ex01/Form.hpp
+++ module05/ex01/Form.hpp
@@ -3,13 +3,21 @@
 # include <iostream>
 # include <string>
 
-class Form
+# include "Bureaucrat.hpp"
+
+class Form : public Bureaucrat
 {
 
 	public:
 
 		Form();
 		~Form();
+
+	private:
+		std::string _name;
+		bool _signed;
+		int _execute;
+		int _sign;
 };
 
 std::ostream &			operator<<( std::ostream & o, Form const & i );
