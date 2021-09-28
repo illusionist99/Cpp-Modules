3a4cbccf86d6a3c4a84e8b25bea942d185c8044b
Mohamed Alaoui <malaoui@e0r3p3.1337.ma>
2021-09-28 15:19:49 +0100

    1%

--- module06/ex02/Base.cpp
+++ module06/ex02/Base.cpp
@@ -1,33 +1,6 @@
 # include "Base.hpp"
 
-Base::Base( void ) {
-
-}
-
-A::A( void ) {
-
-}
-
-B::B( void ) {
-
-}
-
-C::C( void ) {
-
-}
 
 Base::~Base( void ) {
-
-}
-
-A::~A( void ) {
-
-}
-
-B::~B( void ) {
-
-}
-
-C::~C( void ) {
 
 }
--- module06/ex02/Base.hpp
+++ module06/ex02/Base.hpp
@@ -6,29 +6,18 @@ class Base {
 
     public :
 
-        Base( void );
         virtual ~Base( void );
 };
 
 
 class A : public Base {
 
-    public :
-
-        A( void );
-        ~A( void );
 };
 
 class B : public Base {
 
-    public :
-        B( void );
-        ~B( void );
 };
 
 class C : public Base {
 
-    public :
-        C( void );
-        ~C( void );
 };
