aa0fece210a05dc65752643d7be559ee2f0ebb15
Mohamed Alaoui <malaoui@e0r2p3.1337.ma>
2021-10-03 16:30:10 +0100

    1%

--- module08/ex01/span.hpp
+++ module08/ex01/span.hpp
@@ -2,15 +2,65 @@
 
 # include <iostream>
 
-class Nints {
+class Span {
 
+
+    class storeFull : public std::exception {
+        public:
+        const char  *what(void) const throw() {
+            return "Store is Full";
+        }
+    };
+
+    class spanError : public std::exception {
+        public:
+        const char *what(void) const  throw() {
+            return "Not enought element";
+        }
+    };
 
     private:
         unsigned int _n;
-        Nints( void );
+        unsigned int _index;
+        Span( void ) { _index = 0; _mem = NULL;};
+        int         * _mem;
  
     public:
 
-        Nints( unsigned int n );
-        void addNumber()
+        Span( unsigned int n ) {
+
+            _index = 0;
+            _n = n;
+            _mem =  new (std::nothrow) int[n];
+        };
+        ~Span( void ) {
+            if (_mem)
+                delete [] _mem;
+        }
+        void addNumber( int number ) {
+        
+            if (_index > _n)
+                throw Span::storeFull();
+            else {
+                _mem[_index] = number;
+                _index++;
+            }
+        };
+        int shortestSpan( void ) {
+
+            if (_index <= 1)
+                throw Span::spanError();
+            int *tmp = _mem;
+            std::sort(tmp, tmp + _n);
+            return (tmp[1] - tmp[0]);
+        }
+        int longestSpan( void ) {
+
+            if (_index <= 1)
+                throw Span::spanError();
+            int *tmp = _mem;
+            std::sort(tmp, tmp + _n);
+            return (tmp[_n - 1] - tmp[0]);
+        }
+
 };
