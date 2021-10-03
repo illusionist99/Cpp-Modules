4475e8cbe161e8329a76d473b13d5c343893ca31
Mohamed Alaoui <malaoui@e0r2p2.1337.ma>
2021-10-03 21:00:48 +0100

    1%

--- module08/ex02/Mutantstack.cpp
+++ module08/ex02/Mutantstack.cpp
@@ -1,3 +1,2 @@
 #include "Mutantstack.hpp"
 
-
--- module08/ex02/Mutantstack.hpp
+++ module08/ex02/Mutantstack.hpp
@@ -6,13 +6,8 @@
 template < typename T>
 class MutantStack : public std::stack<T>
 {
-
 	public:
-
 		typename std::stack<T>::container_type::iterator begin( void ) { return this->c.begin();};
 		typename std::stack<T>::container_type::iterator end( void ) { return this->c.end();};
 		typedef typename std::stack<T>::container_type::iterator  iterator;
-		
-		MutantStack( void ) {};
-		~MutantStack( void ) {};
 };
